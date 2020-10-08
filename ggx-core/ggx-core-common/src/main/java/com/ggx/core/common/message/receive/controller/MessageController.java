860e563d6473ec916dcdbe80a2088f7bcdba3a0b
zai <379551875@qq.com>
2020-10-08 22:03:58 +0800

    MOD:添加controller/MessageController.java

---
+++ ggx-core/ggx-core-common/src/main/java/com/ggx/core/common/message/receive/controller/MessageController.java
@@ -0,0 +1,5 @@
+package com.ggx.core.common.message.receive.controller;
+
+public interface MessageController {
+	
+}
---
+++ ggx-core/ggx-core-common/src/main/java/com/ggx/core/common/message/receive/handler/ReturnableMessageHandler.java
@@ -0,0 +1,26 @@
+package com.ggx.core.common.message.receive.handler;
+
+
+import com.ggx.core.common.message.MessageData;
+import com.ggx.core.common.message.model.Message;
+
+/**
+ * 带反参的消息处理器
+ * @param <T>
+ * @param <E>
+ * @author zai
+ * 2020-10-7 21:41:19
+ */
+public interface ReturnableMessageHandler<T extends Message, R extends Message> {
+
+	/**
+	 * 处理消息
+	 * @param session
+	 * @param t
+	 * 
+	 * @author zai
+	 * 2019-11-24 22:35:17
+	 */
+	R handle(MessageData<T> messageData);
+	
+}
